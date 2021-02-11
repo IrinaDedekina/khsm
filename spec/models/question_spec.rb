3099e5688e24e46b85e17681d5f23e333a4b8b31
IrinaDedekina <dedekina92@yandex.ru>
2021-02-11 17:06:24 +0300

    test of the question model for text uniqueness

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -19,5 +19,8 @@ RSpec.describe Question, type: :model do
 
     it { should allow_value(14).for(:level) }
     it { should_not allow_value(15).for(:level) }
+
+    subject { FactoryGirl.build(:question, text: "Here is the content", answer1: 2, answer2: 1, answer3: 4, answer4: 3) }
+    it { should validate_uniqueness_of :text }
   end
 end
