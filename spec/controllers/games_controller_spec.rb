80644c933a67e6b3442df30373ed7dd7a8e49663
IrinaDedekina <dedekina92@yandex.ru>
2021-02-26 21:53:13 +0300

    test of the GamesController of the User uses fifty/fifty

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -170,6 +170,24 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
     end
 
+    # тест на отработку "50/50"
+    it 'uses fifty fifty' do
+      # сперва проверяем что в подсказках текущего вопроса пусто
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+      # фигачим запрос в контроллен с нужным типом
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+      expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq 2
+      expect(response).to redirect_to(game_path(game))
+    end
+
     # юзер пытается создать новую игру, не закончив старую
     it 'try to create second game' do
       # убедились что есть игра в работе
